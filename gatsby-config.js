83e87355a57f9052ebc17a2602fbceb4ba98d168
Hank Chen <cchen9299@gmail.com>
2023-11-30 18:00:20 -0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Birth Chart`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
